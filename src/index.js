a42d15fe0d3573c0cfae39713e2b18214f30d797
Rohith <cdrohithm@gmail.com>
2022-03-09 18:56:56 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './components/App'
+
+// function App(){
+//     return (
+//         <h5>Hello</h5>
+//     )
+// }
+
+ReactDOM.render(<App />, document.getElementById('app'))
